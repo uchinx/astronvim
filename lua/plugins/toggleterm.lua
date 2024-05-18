23819a800ca5cf6efb056e27bd31ac018a0bb509
Thinh Nguyen <uchinkaa@gmail.com>
2024-05-18 11:51:16 +0700

    adjust float_term size

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+    "akinsho/toggleterm.nvim",
+    opts = {
+        float_opts = {
+            border = "single",
+            width = 100,
+            height = 30,
+        }
+    }
+}
